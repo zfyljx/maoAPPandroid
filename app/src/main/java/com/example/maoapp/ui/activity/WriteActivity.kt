c5d632e48fa6e19cbda79e32105e267fe8f696bd
hxq <2305461452@qq.com>
2020-04-05 01:13:57 +0800

    添加写分享的ui

--- app/src/main/java/com/example/maoapp/ui/activity/WriteActivity.kt
+++ app/src/main/java/com/example/maoapp/ui/activity/WriteActivity.kt
@@ -1,21 +1,34 @@
 package com.example.maoapp.ui.activity
 
+//import androidx.core.app.ActivityCompat
 import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
+import android.os.Build
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat.checkSelfPermission
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
+import com.afollestad.assent.Permission
+import com.afollestad.assent.runWithPermissions
+import com.amap.api.location.AMapLocationClient
+import com.amap.api.location.AMapLocationClientOption
+import com.amap.api.location.AMapLocationListener
 import com.example.maoapp.R
 import com.example.maoapp.base.BaseInjectActivity
 import com.example.maoapp.contract.WriteContract
 import com.example.maoapp.presenter.WritePresenter
+import com.example.maoapp.utils.LocationResultUtils
+import com.example.maoapp.utils.LocationResultUtils.isLocationProviderEnabled
 import com.qiniu.android.storage.UploadManager
 import com.qw.photo.CoCo
 import com.qw.photo.callback.GetImageCallBack
 import com.qw.photo.pojo.PickResult
 import kotlinx.android.synthetic.main.activity_write.*
-import java.io.File
 
 class WriteActivity : BaseInjectActivity<WritePresenter>(), WriteContract.View{
 
@@ -30,17 +43,17 @@ class WriteActivity : BaseInjectActivity<WritePresenter>(), WriteContract.View{
     private var threeImageUrl=""
     companion object {
         const val WRITE_EXTERNAL_STORAGE = 1
-        const val OPEN_CAMERA = 2
-        const val OPEN_ALBUM = 3
-        const val CROP_IMAGE = 4
     }
 
-    private lateinit var qiniuTokenStr:String
+//    private lateinit var qiniuTokenStr:String
+    private var qiniuTokenStr=""
     private  val  uploadManager= UploadManager()
     lateinit var uri: Uri
-    private val permissionList = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+    private val permissionList = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
     //存储用户拒绝授权的权限
     var permissionTemp: ArrayList<String> = ArrayList()
+    private lateinit var mLocationClient: AMapLocationClient
+    private var mLocationOption = AMapLocationClientOption()
 
 
     override fun getLayoutId(): Int=R.layout.activity_write
@@ -51,9 +64,91 @@ class WriteActivity : BaseInjectActivity<WritePresenter>(), WriteContract.View{
     override fun initPresenter()=mPresenter.attachView(this)
 
 
+//    @ExperimentalStdlibApi
     override fun initWidget() {
-        mPresenter.getQiniuToken()
+           initMap()
+//        mPresenter.getQiniuToken()
+//        verfyStoragePermissions(this)
+        initLinserter()
+
+
+    }
+
+
+
+    fun verfyStoragePermissions(activity:Activity){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+
+            val permissionWrite= checkSelfPermission(activity,"android.permission.WRITE_EXTERNAL_STORAGE")
+            val permissionRead = checkSelfPermission(activity,"android.permission.READ_EXTERNAL_STORAGE")
+//            if (permissionRead != PackageManager.PERMISSION_GRANTED){
+//                Log.d("FFFFFFFF","读存储权限不允许")
+//            }else{
+//                Log.d("TTTTTTTTT","读存储权限已允许")
+//            }
+//            if (permissionWrite != PackageManager.PERMISSION_GRANTED ){
+
+
+//            Thread {
+//                Runnable { ActivityCompat.requestPermissions(activity,permissionList,WriteActivity.WRITE_EXTERNAL_STORAGE) }
+//
+//            }.start()
+
+
+//        }else{
+//            Log.d("TTTTTTTTT","写存储权限已允许")
+//            initLinserter()
+//        }
+    }
+    }
+
+//    @ExperimentalStdlibApi
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//
+//        var isAgree = true
+//        for (i in grantResults.indices){
+//            if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
+//                isAgree = false
+//                val showRequestPermission= shouldShowRequestPermissionRationale(this,permissions[i])
+//                if (showRequestPermission){
+//                    ToastUtils.showToast("你拒绝了读写存储权限")
+//                    Log.d("FFFFFFFFF","拒绝了读写存储权限")
+//                }
+//            }
+//        }
+//        if (isAgree){
+//            Log.d("TTTTTTTT","允许了读写存储权限")
+//            updateImageToQinu()
+//        }
+//
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//
+//    }
+
+//    @ExperimentalStdlibApi
+    private fun initLinserter(){
         pic_one.setOnClickListener {
+            //检查版本是否大于M
+            verfyPermissionsToGallery()
+
+
+        }
+
+
+    get_address.setOnClickListener {
+        locate()
+    }
+
+    }
+
+//    @ExperimentalStdlibApi
+    private fun updateImageToQinu(){
+
         CoCo.with(this)
             .pick()
             .apply()
@@ -62,35 +157,34 @@ class WriteActivity : BaseInjectActivity<WritePresenter>(), WriteContract.View{
                 override fun onSuccess(data: PickResult) {
 //
                     pic_one.setImageURI(data.originUri)
-                    var imagePath=getImagePath(data.originUri,null,null)
+                    var imagePath=data.originUri.toString()
+                    pic_three.setImageURI(Uri.parse(imagePath))
                     Log.d("图片地址",imagePath)
                     Log.d("图片地址",data.localPath)
                     Log.d("图片地址",data.originUri.toString())
-                    uploadManager.put(
-                        File(data.localPath),"zfyljxq.jpg",qiniuTokenStr,
-                        { key, info, response ->
-                            /**
-                             * 用户自定义的内容上传完成后处理动作必须实现的方法
-                             *
-                             * @param key      文件上传保存名称
-                             * @param info     上传完成返回日志信息
-                             * @param response 上传完成的回复内容
-                             */
-                            Log.d("七牛云文件名称",key)
-                            Log.d("七牛云返回结果",info.toString())
-                        },null)
+//                    verfyPerrsion()
+//                    uploadManager.put(
+//                        data.localPath,"zfyljxq.jpg",qiniuTokenStr,
+//                        { key, info, response ->
+//                            /**
+//                             * 用户自定义的内容上传完成后处理动作必须实现的方法
+//                             *
+//                             * @param key      文件上传保存名称
+//                             * @param info     上传完成返回日志信息
+//                             * @param response 上传完成的回复内容
+//                             */
+//                            Log.d("七牛云文件名称",key)
+//                            Log.d("七牛云返回结果",info.toString())
+//                        },null)
+
+
                 }
 
                 override fun onFailed(exception: Exception) {
                     Toast.makeText(this@WriteActivity, "选择异常: $exception", Toast.LENGTH_SHORT).show()
                 }
             })
-        }
-
-
     }
-
-
     override fun loadData() {
 
     }
@@ -104,6 +198,43 @@ class WriteActivity : BaseInjectActivity<WritePresenter>(), WriteContract.View{
         qiniuTokenStr=data
     }
 
+
+    private fun verfyPermissionsToGallery(){
+        //检查版本是否大于M
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissions(
+                    this,
+                    permissionList,
+                    WRITE_EXTERNAL_STORAGE
+                )
+            } else {
+                //权限已经被授权，开启相册
+                updateImageToQinu()
+            }
+        }
+    }
+
+    private fun verfyPerrsion(){
+        //检查版本是否大于M
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissions(
+                    this,
+                    permissionList,
+                    WRITE_EXTERNAL_STORAGE
+                )
+            }
+        }
+    }
 
     /**
      * 解析uri及selection
@@ -121,6 +252,51 @@ class WriteActivity : BaseInjectActivity<WritePresenter>(), WriteContract.View{
         }
         return null
     }
+
+
+    private fun initMap() {
+        mLocationClient = AMapLocationClient(applicationContext)
+        mLocationOption.locationMode = AMapLocationClientOption.AMapLocationMode.Hight_Accuracy
+        mLocationOption.isOnceLocation = true
+
+        //获取最近3s内精度最高的一次定位结果：
+        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+        mLocationOption.isOnceLocationLatest = true
+
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.isNeedAddress = true
+        //关闭缓存机制
+        mLocationOption.isLocationCacheEnable = false
+
+        //声明定位回调监听器
+        val locationListener = AMapLocationListener { loc ->
+            if (null != loc) {
+                //解析定位结果
+                val result = LocationResultUtils.getLocationStr(loc)
+//                locationTv.text = result
+                Log.d("TTTTTTTTTTT",result)
+                write_address.text=result
+            } else {
+                write_address.text="获取定位信息失败"
+            }
+        }
+        //设置定位回调监听
+        mLocationClient.setLocationListener(locationListener)
+    }
+
+    private fun locate() {
+        runWithPermissions(Permission.ACCESS_FINE_LOCATION) {
+            if (isLocationProviderEnabled(this@WriteActivity)) {
+                //给定位客户端对象设置定位参数
+                mLocationClient.setLocationOption(mLocationOption);
+                //启动定位
+                mLocationClient.startLocation()
+            } else {
+                LocationResultUtils.showAlert("本应用需要获取地理位置，请打开获取位置的开关", this)
+            }
+        }
+    }
+
 
 
 }
---
+++ app/src/main/java/com/example/maoapp/utils/LocationResultUtils.kt
@@ -0,0 +1,81 @@
+package com.example.maoapp.utils
+
+import android.app.AlertDialog
+import android.content.Context
+import android.location.LocationManager
+import com.amap.api.location.AMapLocation
+import java.text.SimpleDateFormat
+import java.util.*
+
+object LocationResultUtils {
+    /**
+     * 根据定位结果返回定位信息的字符串
+     */
+    fun getLocationStr(location: AMapLocation): String? {
+        val sb = StringBuffer()
+        //errCode等于0代表定位成功，其他的为定位失败，具体的可以参照官网定位错误码说明
+        if (location.errorCode == 0) {
+            sb.append("定位成功" + "\n")
+            sb.append("定位类型: " + location.locationType + "\n")
+            sb.append("经    度    : " + location.longitude + "\n")
+            sb.append("纬    度    : " + location.latitude + "\n")
+            sb.append("精    度    : " + location.accuracy + "米" + "\n")
+            sb.append("提供者    : " + location.provider + "\n")
+
+            sb.append("速    度    : " + location.speed + "米/秒" + "\n")
+            sb.append("角    度    : " + location.bearing + "\n")
+            // 获取当前提供定位服务的卫星个数
+            sb.append("星    数    : " + location.satellites + "\n")
+            sb.append("国    家    : " + location.country + "\n")
+            sb.append("省            : " + location.province + "\n")
+            sb.append("市            : " + location.city + "\n")
+            sb.append("城市编码 : " + location.cityCode + "\n")
+            sb.append("区            : " + location.district + "\n")
+            sb.append("区域 码   : " + location.adCode + "\n")
+            sb.append("地    址    : " + location.address + "\n")
+            sb.append("兴趣点    : " + location.poiName + "\n")
+            //定位完成的时间
+            sb.append("定位时间: " + formatUTC(location.time, "yyyy-MM-dd HH:mm:ss") + "\n")
+        } else {
+            //定位失败
+            sb.append("定位失败" + "\n")
+            sb.append("错误码:" + location.errorCode + "\n")
+            sb.append("错误信息:" + location.errorInfo + "\n")
+            sb.append("错误描述:" + location.locationDetail + "\n")
+        }
+        //定位之后的回调时间
+        sb.append("回调时间: " + formatUTC(System.currentTimeMillis(), "yyyy-MM-dd HH:mm:ss") + "\n")
+        return sb.toString()
+    }
+
+    private fun formatUTC(l: Long, strPattern: String): String {
+        val sdf = SimpleDateFormat(strPattern, Locale.CHINA)
+        return sdf.format(l)
+    }
+
+    /**
+     * 判断是否开启了GPS或网络定位开关
+     *
+     * @return
+     */
+    fun isLocationProviderEnabled(context: Context): Boolean {
+        var result = false
+        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+            || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            result = true
+        }
+        return result
+    }
+
+    fun showAlert(message: String, context: Context) {
+        val alertDialog = AlertDialog.Builder(context).setMessage(message).setCancelable(false)
+            .setPositiveButton(android.R.string.ok)
+            { dialog, _ ->
+                dialog.dismiss()
+            }
+            .setNegativeButton(android.R.string.cancel) { dialog, _ -> dialog.dismiss() }
+            .create()
+        alertDialog.show()
+    }
+}
